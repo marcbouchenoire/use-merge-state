3ff083707176f8945954145c1f63b14e836ad388
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-12-25 15:04:23 +0100

    Type initial parameter as optional

--- src/hooks/use-merge-state.ts
+++ src/hooks/use-merge-state.ts
@@ -1,8 +1,13 @@
 import { Dispatch, SetStateAction, useCallback, useState } from "react"
 import { getReturnValue, merge } from "../utils"
 
-export const useMergeState = <S>(
-  initialState: S | (() => S)
+interface useMergeState {
+  <S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>]
+  <S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>]
+}
+
+export const useMergeState: useMergeState = <S>(
+  initialState?: any
 ): [S, Dispatch<SetStateAction<S>>] => {
   const [state, setState] = useState<S>(initialState)
 
