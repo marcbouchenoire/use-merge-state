8f825cd774392b4b1dd50206a26b4bbc94987086
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-12-26 20:35:37 +0100

    Fix isPlainObject guard

--- src/guards.ts
+++ src/guards.ts
@@ -1,21 +1,21 @@
 import { PlainFunction, PlainObject, Unpack } from "./types"
 
+export const isArray = Array.isArray
+
 export const isFunction = <T extends PlainFunction>(
   value: T | unknown
 ): value is PlainFunction<Unpack<Parameters<T>>, ReturnType<T>> => {
   return (value ?? null) instanceof Function
 }
 
-export const isArray = Array.isArray
+export const isMap = <T, U>(value: Map<T, U> | unknown): value is Map<T, U> => {
+  return (value ?? null) instanceof Map
+}
 
 export const isPlainObject = <T, U extends PlainObject>(
   value: PlainObject<T> | U | unknown
 ): value is PlainObject<T> | U => {
-  return typeof value === "object" && (value ?? null) instanceof Object
-}
-
-export const isMap = <T, U>(value: Map<T, U> | unknown): value is Map<T, U> => {
-  return (value ?? null) instanceof Map
+  return typeof value === "object" && value?.constructor === Object
 }
 
 export const isSet = <T>(value: Set<T> | unknown): value is Set<T> => {
