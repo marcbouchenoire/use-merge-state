8435e1dd574afaa8ea04b45d8004449b0b0ce041
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2021-12-22 23:56:49 +0100

    Add JSDoc comments

--- src/guards.ts
+++ src/guards.ts
@@ -1,21 +1,46 @@
 import { PlainFunction, PlainObject, Unpack } from "./types"
 
+/**
+ * Return whether the value is an array.
+ *
+ * @param value - The value to be checked.
+ */
 export const isArray = Array.isArray
 
+/**
+ * Return whether the value is a function.
+ *
+ * @param value - The value to be checked.
+ */
 export function isFunction<T extends PlainFunction>(
   value: T | unknown
 ): value is PlainFunction<Unpack<Parameters<T>>, ReturnType<T>> {
   return value instanceof Function
 }
 
+/**
+ * Return whether the value is a Map.
+ *
+ * @param value - The value to be checked.
+ */
 export function isMap<T, U>(value: Map<T, U> | unknown): value is Map<T, U> {
   return value instanceof Map
 }
 
+/**
+ * Return whether the value is a number.
+ *
+ * @param value - The value to be checked.
+ */
 export function isNumber(value: number | unknown): value is number {
   return typeof value === "number"
 }
 
+/**
+ * Return whether the value is a plain object.
+ *
+ * @param value - The value to be checked.
+ */
 export function isPlainObject<T, U extends PlainObject>(
   value: PlainObject<T> | U | unknown
 ): value is PlainObject<T> | U {
@@ -27,6 +52,11 @@ export function isPlainObject<T, U extends PlainObject>(
   )
 }
 
+/**
+ * Return whether the value is a Set.
+ *
+ * @param value - The value to be checked.
+ */
 export function isSet<T>(value: Set<T> | unknown): value is Set<T> {
   return value instanceof Set
 }
--- src/utils/get-return-value.ts
+++ src/utils/get-return-value.ts
@@ -1,5 +1,11 @@
 import { isFunction } from "../guards"
 
+/**
+ * Return a value or its own return value if it's a function.
+ *
+ * @param value - The value or function to return.
+ * @param args - The arguments to provide to the value if it's a function.
+ */
 export function getReturnValue<V, A>(
   value: V | ((...args: A[]) => V),
   ...args: A[]
